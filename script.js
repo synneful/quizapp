6f8894fdd4d429dff165c7f538fee3d45cad849d
Ahmad Akmaluddin bin Bujang <ahmadakmaluddinbujang@gmail.com>
2023-05-29 14:01:40 +0800

    new js syntax

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const questions = [
-    {
-        question: ""
-    }
-]
+function addQuestion(){
+    return addQuestion;
+}
+
+const question = ['first', 'quest', 'answerOne', 'answerTwo', 'answerThree', 'answerFour'];
+
+//to be added
